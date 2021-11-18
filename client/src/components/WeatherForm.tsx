ccd606a9a497dcaf958bf6feb6d2838caab329a0
Jakub Vala <jamm3e3333@gmail.com>
2021-11-18 11:48:56 +0100

    Use form validation

--- client/src/components/WeatherForm.tsx
+++ client/src/components/WeatherForm.tsx
@@ -1,10 +1,47 @@
 import CardComponent from './UI/Card';
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 import classes from './WeatherForm.module.css';
 
 const WeatherFormComponent = () => {
+    let isTouched = false;
+
+    const [city, setCity] = useState('');
+    const [valid, setValid] = useState(true);
+
+    function handleGetWeather(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        isTouched = false;
+    }
+
+    function handleInputCity(e: ChangeEvent<HTMLInputElement>) {
+        setCity(e.target.value);
+    }
+
+    function handleBlurCity() {
+        isTouched = true;
+    }
+
+    useEffect(() => {
+        if(isTouched && !city.trim()) {
+            setValid(false);
+        }
+        else setValid(true);
+    }, [isTouched, city]);
+
     return (
         <CardComponent>
-            Ahoj
+            <form onSubmit={handleGetWeather}>
+                <input 
+                    placeholder="Type city..." 
+                    type="text" 
+                    maxLength={200}
+                    onChange={handleInputCity}
+                    onBlur={handleBlurCity}
+                    value={city}
+                    className={`${valid ? 'invalid' : ''}`}
+                />
+                <button type="submit">Search weather</button>
+            </form>
         </CardComponent>
     )
 }
